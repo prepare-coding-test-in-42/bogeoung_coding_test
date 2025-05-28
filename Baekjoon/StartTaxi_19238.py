7acbaa8b9f16638c09b759cc15ef7bf5b83eba0f
bk <naturalbb360@gmail.com>
2025-05-28 15:31:45 +0900

    feat: MAR WEEK5 StartTaxi Timeout
    - StartTaxi_19238.py

---
+++ Baekjoon/StartTaxi_19238.py
@@ -0,0 +1,111 @@
+from collections import deque
+
+
+class Taxi:
+    def __init__(self, x, y, n):
+        self.x = x
+        self.y = y
+        self.remain_fuel = n
+
+    def move(self, dest_x, dest_y, distance):
+        self.x = dest_x
+        self.y = dest_y
+        self.remain_fuel -= distance
+
+    def fill_fuel(self, distance):
+        self.remain_fuel += distance * 2
+
+class Customer:
+    def __init__(self, x, y, dest_x, dest_y):
+        self.current_x = x
+        self.current_y = y
+        self.dest_x = dest_x
+        self.dest_y = dest_y
+
+
+n = 0
+def input_func():
+    global n
+    n, m, init_fuel = map(int, input().split())
+    road_map = []
+
+    for _ in range(n):
+        road_map.append(list(map(int, input().split())))
+
+    taxi_start_x, taxi_start_y = map(int, input().split())
+    taxi = Taxi(taxi_start_x - 1, taxi_start_y - 1, init_fuel)
+
+    customers = []
+    for _ in range(m):
+        start_x, start_y, dest_x, dest_y = map(int, input().split())
+        customers.append(Customer(start_x - 1, start_y - 1, dest_x - 1, dest_y - 1))
+    customers.sort(key = lambda customer : [customer.current_x, customer.current_y])
+    return road_map, taxi, customers
+
+def calc_distance(road_map, cur_x, cur_y, dest_x, dest_y):
+    queue = deque()
+    visited = [[False for _ in range(n)] for _ in range(n)]
+    move_directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
+    queue.append([cur_x, cur_y, 0])
+    while queue:
+        cur_x, cur_y,cur_move_count = queue.popleft()
+        if cur_x == dest_x and cur_y == dest_y:
+            return cur_move_count
+        visited[cur_x][cur_y] = True
+        for direction in move_directions:
+            new_x, new_y = cur_x + direction[0], cur_y + direction[1]
+            if 0 <= new_x < n and 0 <= new_y < n and road_map[new_x][new_y] == 0 and visited[new_x][new_y] == False:
+                visited[new_x][new_y] = True
+                queue.append([new_x, new_y, cur_move_count + 1])
+    return n * n * 2
+
+def pick_customer(road_map,taxi_x, taxi_y, customers):
+    distances = []
+    for idx, customer in enumerate(customers):
+        distance = calc_distance(road_map, taxi_x, taxi_y, customer.current_x, customer.current_y)
+        distances.append(distance)
+
+    min_distance = min(distances)
+    for idx, distance in enumerate(distances):
+        if distance == min_distance:
+            return [customers.pop(idx), distance]
+
+    return [0, 0]
+
+
+def run(road_map, taxi, customers):
+    while True:
+        if taxi.remain_fuel <= 0 and len(customers) > 0:
+            return -1
+        elif len(customers) <= 0:
+            return taxi.remain_fuel
+
+        # 가장 가까이 있는 손님을 찾음
+        picked_customer, move_distance = pick_customer(road_map, taxi.x, taxi.y, customers)
+        # print(f"picked_customer : {picked_customer}, move_distance : {move_distance}")
+
+        # 손님을 픽업
+        taxi.move(picked_customer.current_x, picked_customer.current_y, move_distance)
+        # print(f"after move taxi's location : [{taxi.x},{taxi.y}]")
+        if taxi.remain_fuel <= 0:
+            return -1
+
+        # 손님을 목적지까지 이동
+        to_destination_distance = calc_distance(road_map, taxi.x, taxi.y, picked_customer.dest_x, picked_customer.dest_y)
+        taxi.move(picked_customer.dest_x, picked_customer.dest_y, to_destination_distance)
+        if taxi.remain_fuel < 0:
+            return -1
+
+        # 연료 충전
+        taxi.fill_fuel(to_destination_distance)
+
+
+def main():
+    road_map, taxi, customers = input_func()
+    answer = run(road_map, taxi, customers)
+    print(answer)
+
+if __name__== "__main__":
+    main()
+
