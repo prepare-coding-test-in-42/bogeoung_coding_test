d13cd79cde0e88dec7c9361d63201283d674781c
bk <naturalbb360@gmail.com>
2025-05-30 17:53:01 +0900

    feat: MAR WEEK5 MonominoDomino2 Problem Solve
    - MonominoDomino2_20061.py

--- Baekjoon/MonominoDomino2_20061.py
+++ Baekjoon/MonominoDomino2_20061.py
@@ -1,6 +1,3 @@
-from Baekjoon.SharkMiddleSchool_21609 import rotate_counter_clockwise
-
-
 class Blocks:
     def __init__(self, row, col):
         self.row = row
@@ -11,32 +8,7 @@ class Blocks:
         return sum(row.count(1) for row in self.map)
 
     def add_block(self, blocks):
-        move_x = 0
-        while True:
-            moved = [[r + move_x + 1, c] for r, c in blocks]
-            if any(r >= self.row or self.map[r][c] != 0 for r, c in moved):
-                break
-            move_x += 1
-
-        for r, c in blocks:
-            self.map[r + move_x][c] = 1
-
-    def check_rows(self):
-        fullfilled_rows = []
-        for row_index in range(self.row):
-            if sum(self.map[row_index]) == self.col:
-                fullfilled_rows.append(row_index)
-
-        for index in fullfilled_rows:
-            self.map.pop(index)
-            self.map.append([0 for _ in range(self.col)])
-        return len(fullfilled_rows)
-
-    def check_light_row(self):
-        for row_index in range(1, -1, -1):
-            if 1 in self.map[row_index]:
-                self.map.pop(row_index)  # 마지막 행 제거
-                self.map.insert(0,[0 for _ in range(self.col)])  # 새로운 행 추가
+        pass
 
     def count_filled_block(self):
         block_count = 0
@@ -47,21 +19,54 @@ class Blocks:
 
 class Green(Blocks):
     def __init__(self):
-        super().__init__(6,4)
+        super().__init__(6, 4)
+
+    def add_block(self, blocks):
+        move_x = -1
+        while True:
+            moved = [[r + move_x + 1, c] for r, c in blocks]
+            if any(r >= self.row or self.map[r][c] != 0 for r, c in moved):
+                break
+            move_x += 1
+
+        for r, c in blocks:
+            self.map[r + move_x][c] = 1
+
+    def check_rows(self):
+        full_filled_rows = []
+        for row_index in range(self.row):
+            if sum(self.map[row_index]) == self.col:
+                full_filled_rows.append(row_index)
+
+        for index in sorted(full_filled_rows):
+            self.map.pop(index)
+            self.map.insert(0, [0 for _ in range(self.col)])
+        return len(full_filled_rows)
+
+    def check_light_row(self):
+        remove_count = 0
+        for row_index in range(1, -1, -1):
+            if 1 in self.map[row_index]:
+                remove_count += 1
+
+        while remove_count > 0:
+            self.map.pop()  # 마지막 행 제거
+            self.map.insert(0, [0 for _ in range(self.col)])  # 새로운 행 추가
+            remove_count -= 1
 
 
 class Blue(Blocks):
     def __init__(self):
-        super().__init__(4,6)
+        super().__init__(4, 6)
 
     def rotate_clockwise(self):
-        self.map =  [list(reversed(col)) for col in zip(*self.map)]
+        self.map = [list(reversed(col)) for col in zip(*self.map)]
 
     def rotate_counter_clockwise(self):
         self.map = [list(row) for row in zip(*self.map)][::-1]
 
     def add_block(self, blocks):
-        move_y = 0
+        move_y = -1
         while True:
             moved = [[r, c + move_y + 1] for r, c in blocks]
             if any(c >= self.col or self.map[r][c] != 0 for r, c in moved):
@@ -69,24 +74,38 @@ class Blue(Blocks):
             move_y += 1
 
         for r, c in blocks:
-            self.map[r][c+move_y] = 1
+            self.map[r][c + move_y] = 1
 
     def check_cols(self):
-        self.rotate_clockwise()
-        cols = super().check_rows()
-        self.rotate_counter_clockwise()
-        return cols
+        full_filled_cols = []
+        for col_index in range(self.col):
+            if all(self.map[row_index][col_index] == 1 for row_index in range(self.row)):
+                full_filled_cols.append(col_index)
+
+        for index in sorted(full_filled_cols):
+            for row_index in range(self.row):
+                self.map[row_index].pop(index)
+                self.map[row_index].insert(0, 0)
+        return len(full_filled_cols)
 
     def check_light_col(self):
+        remove_count = 0
         for col_index in range(1, -1, -1):
             for row_index in range(self.row):
                 if self.map[row_index][col_index] == 1:
-                    self.map[row_index].pop()  # 마지막 행 제거
-                    self.map[row_index].insert(0,0)  # 새로운 행 추가
+                    remove_count += 1
+                    break
+
+        while remove_count > 0:
+            for row_index in range(self.row):
+                self.map[row_index].pop()  # 마지막 행 제거
+                self.map[row_index].insert(0, 0)  # 새로운 행 추가
+            remove_count -= 1
 
     def count_filled_block(self):
         self.rotate_clockwise()
         return super().count_filled_block()
+
 
 def input_func():
     n = int(input())
@@ -97,36 +116,30 @@ def input_func():
 
     return orders
 
+
 def run(orders):
-    block_types = [[[0,0]], [[0,0]], [[0,0], [0,1]], [[0,0], [1,0]]]
+    block_types = [[[0, 0]], [[0, 0]], [[0, 0], [0, 1]], [[0, 0], [1, 0]]]
     blue_blocks = Blue()
     green_blocks = Green()
     score = 0
-    for t,x,y in orders:
-        added_blocks = []
+    for t, x, y in orders:
+        green_added_blocks = []
+        blue_added_blocks = []
         for direction in block_types[t]:
-            new_x, new_y = x + direction[0], y + direction[1]
-            added_blocks.append([new_x, new_y])
+            green_added_blocks.append([direction[0], y + direction[1]])
+            blue_added_blocks.append([x + direction[0], direction[1]])
 
-        green_blocks.add_block(added_blocks)
-        blue_blocks.add_block(added_blocks)
+        green_blocks.add_block(green_added_blocks)
+        blue_blocks.add_block(blue_added_blocks)
 
         score += green_blocks.check_rows()
         score += blue_blocks.check_cols()
-        print("-----Blue-----")
-        for row in blue_blocks.map:
-            print(row)
-        print("-----Green-----")
-        for row in green_blocks.map:
-            print(row)
-        print("================")
 
         green_blocks.check_light_row()
         blue_blocks.check_light_col()
-    print("blue:", blue_blocks.count_filled_block())
-    print("green:", green_blocks.count_filled_block())
 
     return [score, blue_blocks.count_filled_block() + green_blocks.count_filled_block()]
+
 
 def main():
     orders = input_func()
