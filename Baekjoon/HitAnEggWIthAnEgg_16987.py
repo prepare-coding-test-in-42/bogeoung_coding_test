93f2ba6fb0b5187826e58c0f472b2f3fc0873787
bogeoung <naturalbb360@gmail.com>
2025-06-02 19:06:47 +0900

    feat: JUL WEEK1 계란으로 계란치기 Problem Solve
    - HitAnEggWIthAnEgg_16987.py

--- Baekjoon/HitAnEggWIthAnEgg_16987.py
+++ Baekjoon/HitAnEggWIthAnEgg_16987.py
@@ -1,99 +1,60 @@
-from itertools import permutations
-from copy import deepcopy
-
-class Egg():
-    def __init__(self, d, w, i):
-        self.durability = d
-        self.weight = w
-        self.index = i
-        self.is_cracked = False
-
-    def crack_check(self):
-        if self.durability <= 0:
-            self.is_cracked = True
-            return True
-        return False
-
-
-class Eggs():
-    def __init__(self):
-        self.eggs = []
-
-    def add_egg(self, egg):
-        self.eggs.append(egg)
-
-    def get_other_eggs(self, cur_index):
-        return [egg for egg in self.eggs if egg.index != cur_index and egg.crack_check() is False]
-
-    def get_egg(self, finding_egg_index):
-        for egg in self.eggs:
-            if egg.index == finding_egg_index:
-                return egg
-        return None
-
-    def egg_fight(self, egg1_index, egg2_index):
-        egg1, egg2 = self.get_egg(egg1_index), self.get_egg(egg2_index)
-        egg1.durability -= egg2.weight
-        egg2.durability -= egg1.weight
-
-        crack_count = 0
-        if egg1.crack_check():
-            crack_count += 1
-        if egg2.crack_check():
-            crack_count += 1
-        return crack_count
-
-
 n = 0
+max_cracked_num = 0
+
+INDEX_NUM = 0
+DURABILITY_NUM = 1
+WEIGHT_NUM = 2
 
 
 def input_func():
     global n
     n = int(input())
 
-    eggs = Eggs()
+    eggs = []
     for i in range(n):
         d, w = map(int, input().split())
-        eggs.add_egg(Egg(d, w, i))
+        eggs.append([i, d, w])
     return eggs
 
 
-def get_new_sequence_eggs(eggs, order):
-    new_sequence_eggs = Eggs()
-    for idx in order:
-        new_sequence_eggs.add_egg(deepcopy(eggs.eggs[idx]))
-    return new_sequence_eggs
+def dfs(eggs, cur_egg_index, cracked_count):
+    global max_cracked_num
 
+    if cur_egg_index == n:
+        max_cracked_num = max(max_cracked_num, cracked_count)
+        return
 
-def calc_broken_eggs(new_sequence_eggs):
-    broken_egg_count = 0
+    cur_egg = eggs[cur_egg_index]
+    if cur_egg[DURABILITY_NUM] <= 0:
+        dfs(eggs, cur_egg_index + 1, cracked_count)
+        return
+
+    is_fight = False
     for i in range(n):
-        cur_egg = new_sequence_eggs.eggs[i]
-        other_eggs = new_sequence_eggs.get_other_eggs(cur_egg.index)
-        for other_egg in other_eggs:
-            if cur_egg.crack_check():
-                break
-            broken_egg_count += new_sequence_eggs.egg_fight(cur_egg.index, other_egg.index)
+        if i == cur_egg_index or eggs[i][DURABILITY_NUM] <= 0:
+            continue
+        is_fight = True
+        cur_egg[DURABILITY_NUM] -= eggs[i][WEIGHT_NUM]
+        eggs[i][DURABILITY_NUM] -= cur_egg[WEIGHT_NUM]
 
-    return broken_egg_count
+        new_cracked = 0
+        if cur_egg[DURABILITY_NUM] <= 0:
+            new_cracked += 1
+        if eggs[i][DURABILITY_NUM] <= 0:
+            new_cracked += 1
+        dfs(eggs, cur_egg_index + 1, cracked_count + new_cracked)
+        cur_egg[DURABILITY_NUM] += eggs[i][WEIGHT_NUM]
+        eggs[i][DURABILITY_NUM] += cur_egg[WEIGHT_NUM]
 
-
-def run(default_eggs):
-    max_broken_eggs = 0
-    perms = permutations([i for i in range(n)], n)
-    for perm in perms:
-        new_sequence_eggs = get_new_sequence_eggs(default_eggs, perm)
-        # temp = calc_broken_eggs(new_sequence_eggs)
-        # max_broken_eggs = max(max_broken_eggs, temp)
-        max_broken_eggs = max(max_broken_eggs, calc_broken_eggs(new_sequence_eggs))
-        # print(perm, temp)
-    return max_broken_eggs
+    if not is_fight:
+        dfs(eggs, cur_egg_index + 1, cracked_count)
 
 
 def main():
+    global max_cracked_num
     eggs = input_func()
-    answer = run(eggs)
-    print(answer)
+    dfs(eggs, 0, 0)
+    print(max_cracked_num)
 
 
 if __name__ == "__main__":
