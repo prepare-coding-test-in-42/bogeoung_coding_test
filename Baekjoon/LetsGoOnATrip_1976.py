0e330b30f8a0a12d20a65aff9f82b0f55959d063
bogeoung <naturalbb360@gmail.com>
2025-06-04 11:22:14 +0900

    feat: JUL WEEK1 여행 가자 Problem Solve
    - LetsGoOnATrip_1976.py

---
+++ Baekjoon/LetsGoOnATrip_1976.py
@@ -0,0 +1,61 @@
+import copy
+
+n, m = 0, 0
+goal = []
+answer = ""
+
+
+def input_func():
+    global n, m, goal
+    n = int(input())
+    m = int(input())
+
+    roads = []
+
+    for _ in range(n):
+        roads.append(list(map(int, input().split())))
+
+    goal = list(map(int, input().split()))
+
+    for idx, nation_num in enumerate(goal):
+        goal[idx] = nation_num - 1
+
+    return roads
+
+
+def map_update(roads):
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if roads[i][k] and roads[k][j]:
+                    roads[i][j] = 1
+
+def run(roads):
+    global goal
+    n = len(roads)
+    is_possible = True
+
+    map_update(roads)
+
+    for i in range(len(goal) - 1):
+        first_goal = goal[i]
+        next_goal = goal[i-1]
+        if first_goal == next_goal:
+            continue
+        if roads[first_goal][next_goal]:
+            continue
+        else:
+            is_possible = False
+    if is_possible:
+        return "YES"
+    return "NO"
+
+
+def main():
+    roads = input_func()
+    answer = run(roads)
+    print(answer)
+
+
+if __name__ == "__main__":
+    main()
