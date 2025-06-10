a5808467ead8c83662e005316f8753fae9af9e74
bk <naturalbb360@gmail.com>
2025-06-10 19:25:29 +0900

    feat: JUN WEEK2 뱀과 사다리 게임 Problem Solvve
    - SnakeAndLadderGame.py

--- Baekjoon/SnakeAndLadderGame.py
+++ Baekjoon/SnakeAndLadderGame.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class Ladder:
     def __init__(self, s, e):
         self.start = s
@@ -45,50 +47,45 @@ def input_func():
         x, y = map(int, input().split())
         ladders.add(Ladder(x-1, y-1))
         board[x-1] = [1, ladder_index]
-        board[y-1] = [2, ladder_index]
+        # board[y-1] = [2, ladder_index]
 
     snakes = Snakes()
     for snake_index in range(m):
         x, y = map(int, input().split())
         snakes.add(Snake(x-1, y-1))
         board[x-1] = [3, snake_index]
-        board[y-1] = [4, snake_index]
+        # board[y-1] = [4, snake_index]
 
-    # ladders.sort()
-    snakes.sort()
     return ladders, snakes, board
 
 
 def run(ladders, snakes, board):
-    cur_position = 99
-    dice_roll_count = 0
-    while cur_position > 1:
-        # print(f"cur_position : {cur_position}")
-        candidate_ladders = []
-        candidate_location = []
-        for i in range(1, 7):
-            # 탈 수 있는 다리가 6자리 내로 존재하는지 확인
-            new_position = cur_position - i
-            if new_position < 0:
-                break
-            if board[new_position][0] == LADDER_END:  # 다리의 도착지점이라면
-                ladder_index = board[new_position][1]
-                candidate_ladders.append(ladders.ladders[ladder_index])
-                continue
-            if board[new_position][0] in [2, 3]:  # 다리의 시작지점이거나 뱀의 시작, 도착지점이라면
-                continue
-            candidate_location.append(i)
+    queue = deque()
+    cur_move_times = 0
+    queue.append([0, cur_move_times])
+    visited = [False for _ in range(100)]
+    while queue:
+        cur_location, cur_move_times = queue.popleft()
+        if cur_location >= 99:
+            return cur_move_times
 
-        # 다리가 존재한다면, 가장 멀리 갈 수 있는 다리로 이동
-        if candidate_ladders:
-            candidate_ladders.sort(key=lambda x: x.start)
-            cur_position = candidate_ladders[0].start
-            # print(f"move with ladders")
-        else:
-            cur_position -= candidate_location[-1]
-        # print(f"dice_updated")
-        dice_roll_count += 1
-    return dice_roll_count
+        for i in range(1, 7):
+            new_location = cur_location + i
+            if new_location >= 100:
+                continue
+            # 이동할 위치가 다리의 시작이라면
+            if board[new_location][0] == 1:
+                ladder_index = board[new_location][1]
+                new_location = ladders.ladders[ladder_index].end
+            # 이동할 위치가 뱀의 시작이라면
+            elif board[new_location][0] == 3:
+                snake_index = board[new_location][1]
+                new_location = snakes.snakes[snake_index].end
+            if visited[new_location]:
+                continue
+            visited[new_location] = True
+            queue.append([new_location, cur_move_times + 1])
+    return cur_move_times
 
 
 def main():
