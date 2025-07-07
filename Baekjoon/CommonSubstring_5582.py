b614afe5999c3e8616e17639f8fabd40c7a38ac3
bk <naturalbb360@gmail.com>
2025-07-07 17:21:39 +0900

    feat: JUL WEEK2 공통 부분 문자열 Problem Solve
    - CommonSubstring_5582.py

--- Baekjoon/CommonSubstring_5582.py
+++ Baekjoon/CommonSubstring_5582.py
@@ -1,27 +1,26 @@
 def run(input1, input2):
-    longest_common_substring = 0
-    start_index = 0
-    for i in range(1, len(input1)): # 부분 문자열의 길이
-        for j in range(start_index + len(input1) - i): # 부분 문자열의 시작점
-            substring = input1[j: j+i]
-            if substring in input2:
-                longest_common_substring = i
-                break
-        if i != longest_common_substring:
-            start_index = i + 1
-    return longest_common_substring
+    n, m = len(input1), len(input2)
+    dp = [0] * (m+1)
+    result = 0
+
+    for i in range(1, n+1):
+        prev = 0
+        for j in range(1, m+1):
+            temp = dp[j]
+            if input1[i-1] == input2[j-1]:
+                dp[j] = prev + 1
+                result = max(result, dp[j])
+            else:
+                dp[j] = 0
+            prev = temp
+    return result
 
 
 def main():
     input1 = input()
     input2 = input()
 
-    answer = 0
-
-    if len(input1) < len(input2):
-        answer = run(input1, input2)
-    else:
-        answer = run(input2, input1)
+    answer = run(input1, input2)
     print(answer)
 
 if __name__ == "__main__":
