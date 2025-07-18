84eaa3e87895a39d413ec045d9531ffaf518990e
bogeoung <naturalbb360@gmail.com>
2025-07-18 20:35:31 +0900

    feat: JUL WEEK3 동전 2 Problem Solving
    - Coin2_2294.py

--- Baekjoon/Coin2_2294.py
+++ Baekjoon/Coin2_2294.py
@@ -8,29 +8,28 @@ def input_func():
 
     return k, sorted(list(coins), reverse= True)
 
+
+def dp(make_coin, coins):
+    dp_list = [[] for _ in range(make_coin + 1)]
+    dp_list[0] = [[0]]
+
+    for coin in coins:
+        for i in range(coin, make_coin + 1):
+            for before_list in dp_list[i-coin]:
+                candidate_list = before_list[:]
+                candidate_list.append(coin)
+                dp_list[i].append(candidate_list)
+
+    min_length = len(dp_list[make_coin][0])
+
+    for candidate_list in dp_list[make_coin]:
+        if len(candidate_list) < min_length:
+            min_length = len(candidate_list)
+    return min_length - 1
+
 def main():
     make_coin, coins = input_func()
-
-    min_total_count = 100000
-
-    while coins:
-        remain_amount = make_coin
-        coin_total_count = 0
-        for coin in coins:
-            cur_coin_count = remain_amount // coin
-            coin_total_count += cur_coin_count
-            remain_amount -= cur_coin_count * coin
-
-            if remain_amount == 0:
-                break
-
-        if coin_total_count > 0:
-            min_total_count = min(min_total_count, coin_total_count)
-        coins.pop(0)
-    if min_total_count == 10000:
-        print("-1")
-    else:
-        print(min_total_count)
+    print(dp(make_coin, coins))
 
 if __name__ == "__main__":
     main()
