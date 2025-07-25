1f749f44888d41bc8e93bef065da86b881ed7f2e
bk <naturalbb360@gmail.com>
2025-07-25 16:21:41 +0900

    feat: JUL WEEK3 동전 2 Problem Solve
    - Coin2_2294.py

--- Baekjoon/Coin2_2294.py
+++ Baekjoon/Coin2_2294.py
@@ -10,22 +10,17 @@ def input_func():
 
 
 def dp(make_coin, coins):
-    dp_list = [[] for _ in range(make_coin + 1)]
-    dp_list[0] = [[0]]
+    dp_list = [1000000 for _ in range(make_coin + 1)]
+    dp_list[0] = 0
 
     for coin in coins:
         for i in range(coin, make_coin + 1):
-            for before_list in dp_list[i-coin]:
-                candidate_list = before_list[:]
-                candidate_list.append(coin)
-                dp_list[i].append(candidate_list)
+            dp_list[i] = min(dp_list[i], dp_list[i-coin] + 1)
 
-    min_length = len(dp_list[make_coin][0])
 
-    for candidate_list in dp_list[make_coin]:
-        if len(candidate_list) < min_length:
-            min_length = len(candidate_list)
-    return min_length - 1
+    if dp_list[make_coin] == 1000000:
+        return -1
+    return dp_list[make_coin]
 
 def main():
     make_coin, coins = input_func()
